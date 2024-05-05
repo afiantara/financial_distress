ae2c0c159733abb6d02469d3f7280e3e1dd4eb57
afiantara <afiantara@gmail.com>
2024-05-05 15:35:27 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,93 +1,155 @@
-from mpl_toolkits.mplot3d import Axes3D
-from sklearn.preprocessing import StandardScaler
-import matplotlib.pyplot as plt # plotting
-import numpy as np # linear algebra
-import os # accessing directory structure
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import sqlite3
 from sqlalchemy import create_engine
 
-# Distribution graphs (histogram/bar graph) of column data
-def plotPerColumnDistribution(df, nGraphShown, nGraphPerRow):
-    nunique = df.nunique()
-    print(nunique)
-    df = df[[col for col in df if nunique[col] > 1 and nunique[col] < 10000]] # For displaying purposes, pick columns that have between 1 and 50 unique values
-    nRow, nCol = df.shape
-    print(df.shape)
-    columnNames = list(df)
-    nGraphRow =int((nCol + nGraphPerRow - 1) / nGraphPerRow)
-    print(nGraphRow)
-    print(6 * nGraphPerRow, 8 * nGraphRow)
-    plt.figure(num = None, figsize = (6 * nGraphPerRow, 8 * nGraphRow), dpi = 80, facecolor = 'w', edgecolor = 'k')
-    for i in range(min(nCol, nGraphShown)):
-        print(i)
-        plt.subplot(nGraphRow, nGraphPerRow, i + 1)
-        columnDf = df.iloc[:, i]
-        if (not np.issubdtype(type(columnDf.iloc[0]), np.number)):
-            valueCounts = columnDf.value_counts()
-            valueCounts.plot.bar()
-        else:
-            columnDf.hist()
+def treatoutliers(df, columns=None, factor=1.5, method='IQR', treament='cap'):
+    """
+    Removes the rows from self.df whose value does not lies in the specified standard deviation
+    :param columns:
+    :param in_stddev:
+    :return:
+    """
+#     if not columns:
+#         columns = self.mandatory_cols_ + self.optional_cols_ + [self.target_col]
+    if not columns:
+        columns = df.columns
+    
+    for column in columns:
+        if method == 'STD':
+            permissable_std = factor * df[column].std()
+            col_mean = df[column].mean()
+            floor, ceil = col_mean - permissable_std, col_mean + permissable_std
+        elif method == 'IQR':
+            Q1 = df[column].quantile(0.25)
+            Q3 = df[column].quantile(0.75)
+            IQR = Q3 - Q1
+            floor, ceil = Q1 - factor * IQR, Q3 + factor * IQR
+        
+        if treament == 'remove':
+            df = df[(df[column] >= floor) & (df[column] <= ceil)]
+        elif treament == 'cap':
+            df[column] = df[column].clip(floor, ceil)
+            
+    return df
 
-        plt.ylabel('counts')
-        plt.xticks(rotation = 90)
-        plt.title(f'{columnNames[i]} (column {i})')
-
-    plt.tight_layout(pad = 1.0, w_pad = 1.0, h_pad = 1.0)
+def show_outliers(df,columns):
+    booxplot = df.boxplot(column=columns)  
     plt.show()
 
-# Correlation matrix
-def plotCorrelationMatrix(df, graphWidth):
-    filename = df.dataframeName
-    df = df.dropna('columns') # drop columns with NaN
-    df = df[[col for col in df if df[col].nunique() > 1]] # keep columns where there are more than 1 unique values
-    if df.shape[1] < 2:
-        print(f'No correlation plots shown: The number of non-NaN or constant columns ({df.shape[1]}) is less than 2')
-        return
-    corr = df.corr()
-    plt.figure(num=None, figsize=(graphWidth, graphWidth), dpi=80, facecolor='w', edgecolor='k')
-    corrMat = plt.matshow(corr, fignum = 1)
-    plt.xticks(range(len(corr.columns)), corr.columns, rotation=90)
-    plt.yticks(range(len(corr.columns)), corr.columns)
-    plt.gca().xaxis.tick_bottom()
-    plt.colorbar(corrMat)
-    plt.title(f'Correlation Matrix for {filename}', fontsize=15)
-    plt.show()
+def add_data(df,cols):
+    #df['year'] = df['PeriodeData'].dt.year
+    for col in cols:
+        df['D_' + col] = df.groupby(['SandiBPR'])[col].diff()
+        df['G_'+col]=df.groupby(['SandiBPR'])[col].pct_change(periods=1)*100
+        df['MA_' + col] = df.groupby(['SandiBPR'])[col].transform(lambda x: x.rolling(2, 1).mean())
+    df=df.dropna() 
+    return df
 
-# Scatter and density plots
-def plotScatterMatrix(df, plotSize, textSize):
-    df = df.select_dtypes(include =[np.number]) # keep only numerical columns
-    # Remove rows and columns that would lead to df being singular
-    df = df.dropna('columns')
-    df = df[[col for col in df if df[col].nunique() > 1]] # keep columns where there are more than 1 unique values
-    columnNames = list(df)
-    if len(columnNames) > 10: # reduce the number of columns for matrix inversion of kernel density plots
-        columnNames = columnNames[:10]
-    df = df[columnNames]
-    ax = pd.plotting.scatter_matrix(df, alpha=0.75, figsize=[plotSize, plotSize], diagonal='kde')
-    corrs = df.corr().values
-    for i, j in zip(*plt.np.triu_indices_from(ax, k = 1)):
-        ax[i, j].annotate('Corr. coef = %.3f' % corrs[i, j], (0.8, 0.2), xycoords='axes fraction', ha='center', va='center', size=textSize)
-    plt.suptitle('Scatter and Density Plot')
-    plt.show()
+def read():
+    df = pd.read_excel("./Data Train.xlsx")
+    df=df.drop(['BDL'], axis=1)
+    # remove duplicate values in ProductID column
+    df = df.drop_duplicates(subset=['PeriodeData','SandiBPR'], keep='first')
 
-def insert(data: pd.DataFrame):
-    engine = create_engine("sqlite:///{}".format("distress.db"))
-    with engine.connect() as connection:
-        data.to_sql('rasio', engine, if_exists='replace', index=False)
+    df['KPMM'] = pd.to_numeric(df['KPMM'], errors='coerce')
+    df['ROA'] = pd.to_numeric(df['ROA'], errors='coerce')
+    df['BOPO'] = pd.to_numeric(df['BOPO'], errors='coerce')
+    df['CR'] = pd.to_numeric(df['CR'], errors='coerce')
+    df['NNPL'] = pd.to_numeric(df['NNPL'], errors='coerce')
+    df = df.sort_values(['SandiBPR','PeriodeData']) #first sorte the periode and sandiBPR
+    df = df.bfill() #if nan fill with the known previous value
+    return df
+
+def readCIU():
+    df = pd.read_excel("./ciu.xlsx")
+    df['CIU']=1.0
+    df['SandiBPR']=df['SandiBank']
+    return df
+        
+def preprocessing():
+    df=read()
+    df = add_data(df,['KPMM','ROA','BOPO','CR','NNPL'])
+    dfCIU=readCIU()
+    df['CIU'] = df['SandiBPR'].map(dfCIU.set_index('SandiBPR')['CIU']).fillna(0)
+    
+    #add flag OJK BDP
+    # flag =0 is normal
+    # flag =1 is BDP
+    # flag =2 is BDR
+    
+    #Flag== 1
+    #a. TKS dengan PK 5 (lima) selama 2 (dua) periode berturut-turut;
+    #b. CR rata-rata 3 (tiga) bulan terakhir kurang dari 5%(lima persen); dan/atau
+    #c. rasio KPMM kurang dari 12% (dua belas persen).
+    
+    #flag == 0
+    #a. TKS minimal PK 4 (empat);
+    #b. CR rata-rata 3 (tiga) bulan terakhir paling sedikit 5% (lima persen); dan
+    #c. rasio KPMM paling sedikit 12% (dua belas persen).
+    
+    #Flag==2
+    #a. rasio KPMM menjadi kurang dari sama dengan 2% (dua persen) dan/atau 
+    #b. CR rata-rata selama 3 (tiga) bulan terakhir menjadi kurang dari sama dengan 1% (satu persen); atau
+    #atau
+    #a. mengalami penurunan rasio KPMM dan/atau CR; dan
+    #b. tidak mampu meningkatkan rasio KPMM menjadi paling sedikit 12% (dua belas persen) dan/atau CR rata-rata 3 (tiga) bulan terakhir paling sedikit 5% (lima persen).
+    df_KPMM = df['KPMM']
+    df_MACR = df['MA_CR']
+    df_DKPMM = df['D_KPMM']
+    df_DCR = df['D_CR']
+    
+    # List of conditions
+    conditions = [
+        ((df_KPMM <=2) & (df_MACR <=1)) | ((df_DKPMM<0) & (df_DCR<0) & (df_KPMM<=12) & (df_MACR<=5))
+        ,(df_KPMM <12) & (df_MACR <5)
+        
+
+    ]
+
+    # List of values to return
+    choices  = [2.0, 1.0]
+    
+    # create a new column in the DF based on the conditions
+    df['flag'] = np.select(conditions, choices, 0.0)
+    
+    df_flag=df['flag']
+    df_CIU = df['CIU']
+    
+    conditions=[
+        ((df_flag>0) & (df_CIU>0))
+        ,((df_flag==0) & (df_CIU==0))
+    ]
+    choices=[0.0,0.0]
+    
+    df['anomali'] = np.select(conditions,choices,1.0)
+    return df
+
+def drop_tosql(df):
+    # Create your connection.
+    cnx = sqlite3.connect(':memory:')
+    disk_engine = create_engine('sqlite:///data_bpr.db')
+    df.to_sql('ratio', disk_engine, if_exists='replace')
+    
+def show_case(df):
+    import seaborn as sns
+    data = df[df['SandiBPR']==600551].reset_index(drop=True)
+    print(data)
+    ax = sns.lineplot(x='PeriodeData',y='KPMM', data=data,label = 'KPMM')
+    ax.axhline(y = 2,linestyle = "dashed", color = "red",label="KPMM BDR Regulation")
+    ax.axhline(y = 12,linestyle = "dashed", color = "orange",label="KPMM BDP Regulation")
+     
+    ax1= sns.lineplot(x='PeriodeData', y='MA_CR', data=data,label='MA_CR')
+    ax1.axhline(y = 1,linestyle = "dashed", color = "blue",label="MA_CR BDR Regulation")
+    ax.axhline(y = 5,linestyle = "dashed", color = "yellow",label="MA_CR BDP Regulation")
+    ax2= sns.lineplot(x='PeriodeData', y='D_KPMM', data=data,label='D_KPMM')
+    ax3= sns.lineplot(x='PeriodeData', y='D_CR', data=data,label='D_CR')
+    plt.legend()
+    plt.title('{}'.format('600551 - Perumda BPR Bank Purworejo'))
+    plt.show()
 
 if __name__=="__main__":
-    nRowsRead = 1000 # specify 'None' if want to read whole file
-    # Financial Distress.csv has 3673 rows in reality, but we are only loading/previewing the first 1000 rows
-    #df1 = pd.read_csv('./dataset final.xlsx', delimiter=',', nrows = nRowsRead)
-    df1 = pd.read_excel('./dataset final.xlsx')
-    from ydata_profiling import ProfileReport
-    profile = ProfileReport(df1, title='Pandas Profiling Report', html={'style':{'full_width':False}})
-    profile.to_file(output_file="REPORT.html")
-    #insert(df1)
-    #df1.dataframeName = 'dataset final.xlsx'
-    #nRow, nCol = df1.shape
-    #print(f'There are {nRow} rows and {nCol} columns')
-    #print(df1.head())
-    #plotPerColumnDistribution(df1, 10, 5)
-    #plotCorrelationMatrix(df1, 21)
-    #plotScatterMatrix(df1, 20, 10)
+    df=preprocessing()
+    show_case(df)    
+    drop_tosql(df)
